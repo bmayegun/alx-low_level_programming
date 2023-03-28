02138dff136b523130c38f0677a0768f0772a85a
Bisoye <bmayegun@gmail.com>
2023-03-28 15:58:50 -0700

    Added exercise files for pointers and arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the supplied int parameter and points it to 98
+ *
+ * @n: pointer to variable to be updated
+ */
+void reset_to_98(int *n)
+{
+	*n = 98
+}
