18f178d8558798bf61ab0dba2f8bea106001eec6
Bisoye <bmayegun@gmail.com>
2023-04-03 21:59:44 -0700

    Added files

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_memset fills the firrst n bytes of memeory area pointed to by s with b
+ * @s: memory area to be filled
+ * @b: char to put in memory
+ * @n: number of times to copy
+ *
+ * Return: pointer to s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
