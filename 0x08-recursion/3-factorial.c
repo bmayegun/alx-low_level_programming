bdffbe1816a384b036497dfb895176f9e3acb81f
Bisoye <bmayegun@gmail.com>
2023-04-06 23:01:48 -0700

    Fixed betty warnings

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,11 +7,11 @@
  * Return: the factorial of the number
  */
 
-int factorial(n)
+int factorial(int n)
 {
 	if (n < 1)
 		return (-1);
 	if (n == 1)
 		return (1);
-	return (n *factorial(n - 1));
+	return (n * factorial(n - 1));
 }
