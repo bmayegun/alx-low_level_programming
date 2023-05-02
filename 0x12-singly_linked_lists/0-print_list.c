80f7669b208262e35ffc3cc9832433dcb43927c3
Bisoye <bmayegun@gmail.com>
2023-05-02 14:40:49 -0700

    Singly linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - function to print a linked list
+ * @h: pointer to head of the list
+ *
+ * Return: number of nodes 
+ * /
+size_t print_list(const list_t *h){
+
+	size_t size = 0;
+
+	while (h)
+	{
+		if (h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		
+		size++;
+		h = h->next;
+	}
+
+	return (size);
+}
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: double pointer to the list
+ * @str: new string to set in the new node
+ *
+ * Return: the address of the new node on success, or NULL on failure
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode;
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	newNodde = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	newNode->str = strdup(str);
+	newNode->len = len;
+	newNode->next = (*head);
+	(*head) = newNode;
+
+	return (*head);
+}
